cbe232fb97c0804a1bda9b0ffa8b5a48e2de06b1
Yaseen-Davids <yaseendavids477@gmail.com>
2020-05-14 09:56:38 +0200

    error handling

--- app.js
+++ app.js
@@ -67,8 +67,13 @@ app.use(function (req, res, next) {
 });
 
 app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.send(err.message ? err.message : "Error unexpected!").end();
+  res.render("error");
 });
 
 module.exports = app;
