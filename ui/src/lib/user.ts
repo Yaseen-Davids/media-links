14fd2ec25264174742b959b8551768eacc3677ce
Yaseen-Davids <yaseendavids477@gmail.com>
2020-10-13 16:20:06 +0200

    app code rearrange 8

--- ui/src/lib/user.ts
+++ ui/src/lib/user.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const whoami = async (): Promise<any> => await axios.get("/users/whoami/dashjsdahjdsa");
+export const whoami = async (): Promise<any> => await fetch("/users/whoami/dashjsdahjdsa", { method: "GET" });
 
 export const login = async (fields: { username: string; password: string }) =>
   await axios.post("/users/login", fields, {
