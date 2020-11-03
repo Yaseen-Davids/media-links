a274dde76f1b961fb5264b3842ea631c8b3684d5
Yaseen-Davids <yaseendavids477@gmail.com>
2020-11-03 19:13:55 +0200

    reset

--- packages/ui/src/lib/base.ts
+++ packages/ui/src/lib/base.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 
 type Methods = "PUT" | "DELETE" | "GET" | "POST";
 
-export const apiURL = "https://medialinks.herokuapp.com/api";
+export const apiURL = "http://medialinks.dev";
 
-export const request = async (url: string, options: { method: Methods, body?: any }) => {
+export const request = async (url: string, options: { method: Methods; body?: any }) => {
   return await axios({
     method: options.method,
     url: apiURL + url,
-    data: options.body
+    data: options.body,
   });
-}
+};
