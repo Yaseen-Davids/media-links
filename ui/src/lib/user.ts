27345a1757a27a408364527bf258a6712e5b0cd0
Yaseen-Davids <yaseendavids477@gmail.com>
2020-12-03 22:37:09 +0200

    prod changes

--- ui/src/lib/user.ts
+++ ui/src/lib/user.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-export const whoami = async (): Promise<any> => await axios.get("/users/whoami");
+export const whoami = async (): Promise<any> => await axios.get("/api/users/whoami");
 
 export const login = async (fields: { username: string; password: string }) =>
-  await axios.post("/users/login", fields, {
+  await axios.post("/api/users/login", fields, {
     withCredentials: true,
   });
 
-export const logout = async (): Promise<any> => await axios.get("/users/logout");
+export const logout = async (): Promise<any> => await axios.get("/api/users/logout");
